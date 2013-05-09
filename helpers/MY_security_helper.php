538f76f3e30f509368b8d1285a72aa99d837c28c
groucho75 <groucho75@email.it>
2013-05-09 18:07:10 +0300

    Update MY_security_helper.php

--- helpers/MY_security_helper.php
+++ helpers/MY_security_helper.php
@@ -3,10 +3,11 @@
 /**
  * Security helper extended
  *
- * @package   CodeIgniter
+ * @package 	CodeIgniter
  * @subpackage 	Helper
  * @category 	Security
  */
+
 
 /**
  * Append CSRF token to URL
@@ -21,14 +22,14 @@ if ( ! function_exists('csrf_site_url'))
 	{	
 		$CI =& get_instance();
 		
-		if ( $CI->config->item('csrf_protection') == false ) {
-			 log_message('error', 'Helper: Security - csrf_protection is false');
-		}
-		
 		$CI->load->helper('url');
 		
 		// Make URI relative: trim base url
 		if ( $uri != '' ) $uri = str_replace( site_url(), '', $uri );
+		
+		if ( $CI->config->item('csrf_protection') == false ) {
+			 log_message('error', 'Helper: Security - csrf_protection is false');
+		}
 		
 		$csrf_name 	= config_item('csrf_token_name');
 		$csrf_value	= $CI->security->get_csrf_hash();
@@ -53,13 +54,16 @@ if ( ! function_exists('csrf_site_url'))
  * Check CSRF token appended to URL
  *
  * @access	public
+ * @param	string	the url to check, or current URI if empty
  * @return	bol
  */
 if ( ! function_exists('check_csrf_url'))
 {
-	function check_csrf_url()
+	function check_csrf_url( $uri='' )
 	{	
 		$CI =& get_instance();
+		
+		$CI->load->helper('url');
 		
 		if ( $CI->config->item('csrf_protection') == false )
 		{
@@ -67,19 +71,37 @@ if ( ! function_exists('check_csrf_url'))
 			return TRUE;
 		}
 		
-		$segments = $CI->uri->total_segments();
+		if ( $uri == '' )
+		{	
+			// Get current URI	
+			$segments = $CI->uri->total_segments();
+		}
+		else
+		{
+			// Make URI relative: trim base url
+			$uri = trim( str_replace( site_url(), '', $uri ), '/' );
+			$uri_array = explode( '/', $uri );
+			$segments = count( $uri_array );
+		}
 		
-		// No segment: false
+		// Less than 2 segments? Error
 		if ( $segments < 2 )
 		{
 			$CI->security->csrf_show_error();
 		}
 		
 		// Get appended CSRF
-		$appended_csrf_name = $CI->uri->segment( ($segments-1) );
-		$appended_csrf_value = $CI->uri->segment( $segments );
-
-
+		if ( $uri == '' )
+		{					
+			$appended_csrf_name = $CI->uri->segment( ($segments-1) );
+			$appended_csrf_value = $CI->uri->segment( $segments );
+		}
+		else
+		{
+			$appended_csrf_name = $uri_array[ ($segments-2) ];
+			$appended_csrf_value = $uri_array[ ($segments-1) ];
+		}
+				
 		// Get CSRF values form config
 		$csrf_token_name = config_item('csrf_token_name');
 		$csrf_cookie_name = config_item('csrf_cookie_name');
